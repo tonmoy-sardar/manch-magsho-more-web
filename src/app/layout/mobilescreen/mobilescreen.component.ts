3b8b99209c29c55b042f4037f4e0af565d296580
Anant Singh <tonomy>
2019-06-21 15:04:57 +0530

    new page mobile

--- src/app/layout/mobilescreen/mobilescreen.component.ts
+++ src/app/layout/mobilescreen/mobilescreen.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { flattenStyles } from '@angular/platform-browser/src/dom/dom_renderer';
+import { ProductService } from '../../core/services/product.service';
 @Component({
   selector: 'app-mobilescreen',
   templateUrl: './mobilescreen.component.html',
   styleUrls: ['./mobilescreen.component.scss']
 })
 export class MobilescreenComponent implements OnInit {
-
-  constructor() { }
+  productList:any=[];
+  visibleKey:boolean =false;
+  constructor(
+    public productService:ProductService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
-
+  gotoDetails(id) {
+    this.router.navigate(['product/details', id]);
+  }
 }
